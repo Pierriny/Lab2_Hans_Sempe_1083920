916830b568c49dace859d7699a6639cb8f7278b9
Pierriny <70544564+Pierriny@users.noreply.github.com>
2020-09-15 17:30:40 -0600

    Copia de seguridad No.1

--- Lab2_Hans_Sempe_1083920/MyForm1.h
+++ Lab2_Hans_Sempe_1083920/MyForm1.h
@@ -1,3 +1,6 @@
+#include <iostream>
+#include <time.h>
+#include <stdlib.h>
 #pragma once
 
 namespace Lab2HansSempe1083920 {
@@ -8,6 +11,7 @@ namespace Lab2HansSempe1083920 {
 	using namespace System::Windows::Forms;
 	using namespace System::Data;
 	using namespace System::Drawing;
+	using namespace std;
 
 	/// <summary>
 	/// Resumen de MyForm1
@@ -34,16 +38,20 @@ namespace Lab2HansSempe1083920 {
 				delete components;
 			}
 		}
-	private: System::Windows::Forms::DataGridView^ dataGridView1;
+	private: System::Windows::Forms::DataGridView^ Campo;
+	protected:
+
 	protected:
 	private: System::Windows::Forms::Button^ button1;
 	private: System::Windows::Forms::Button^ button2;
+	private: System::Windows::Forms::Timer^ timer1;
+	private: System::ComponentModel::IContainer^ components;
 
 	private:
 		/// <summary>
 		/// Variable del diseñador necesaria.
 		/// </summary>
-		System::ComponentModel::Container ^components;
+
 
 #pragma region Windows Form Designer generated code
 		/// <summary>
@@ -52,21 +60,23 @@ namespace Lab2HansSempe1083920 {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			this->components = (gcnew System::ComponentModel::Container());
 			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(MyForm1::typeid));
-			this->dataGridView1 = (gcnew System::Windows::Forms::DataGridView());
+			this->Campo = (gcnew System::Windows::Forms::DataGridView());
 			this->button1 = (gcnew System::Windows::Forms::Button());
 			this->button2 = (gcnew System::Windows::Forms::Button());
-			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->BeginInit();
+			this->timer1 = (gcnew System::Windows::Forms::Timer(this->components));
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->Campo))->BeginInit();
 			this->SuspendLayout();
 			// 
-			// dataGridView1
+			// Campo
 			// 
-			this->dataGridView1->BackgroundColor = System::Drawing::Color::White;
-			this->dataGridView1->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
-			this->dataGridView1->Location = System::Drawing::Point(12, 149);
-			this->dataGridView1->Name = L"dataGridView1";
-			this->dataGridView1->Size = System::Drawing::Size(892, 107);
-			this->dataGridView1->TabIndex = 0;
+			this->Campo->BackgroundColor = System::Drawing::Color::White;
+			this->Campo->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
+			this->Campo->Location = System::Drawing::Point(12, 149);
+			this->Campo->Name = L"Campo";
+			this->Campo->Size = System::Drawing::Size(892, 107);
+			this->Campo->TabIndex = 0;
 			// 
 			// button1
 			// 
@@ -79,6 +89,7 @@ namespace Lab2HansSempe1083920 {
 			this->button1->TabIndex = 1;
 			this->button1->Text = L"START";
 			this->button1->UseVisualStyleBackColor = false;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm1::button1_Click);
 			// 
 			// button2
 			// 
@@ -92,6 +103,11 @@ namespace Lab2HansSempe1083920 {
 			this->button2->Text = L"Reiniciar";
 			this->button2->UseVisualStyleBackColor = false;
 			// 
+			// timer1
+			// 
+			this->timer1->Interval = 1000;
+			this->timer1->Tick += gcnew System::EventHandler(this, &MyForm1::timer1_Tick);
+			// 
 			// MyForm1
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
@@ -100,13 +116,153 @@ namespace Lab2HansSempe1083920 {
 			this->ClientSize = System::Drawing::Size(916, 275);
 			this->Controls->Add(this->button2);
 			this->Controls->Add(this->button1);
-			this->Controls->Add(this->dataGridView1);
+			this->Controls->Add(this->Campo);
 			this->Name = L"MyForm1";
 			this->Text = L"MyForm1";
-			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->Campo))->EndInit();
 			this->ResumeLayout(false);
 
 		}
 #pragma endregion
-	};
+
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+
+
+		int Num_Fila = 2, Num_Columns = 70;
+
+		for (int i = 0; i < Num_Columns; i++) {
+
+			DataGridViewColumn^ nuevacolumna = gcnew DataGridViewColumn();
+			nuevacolumna->Width = 20;
+
+
+			DataGridViewCell^ cellTemplate = gcnew DataGridViewTextBoxCell();
+			nuevacolumna->CellTemplate = cellTemplate;
+
+
+			Campo->Columns->Add(nuevacolumna);
+		}
+
+		for (int i = 1; i < Num_Fila; i++) {
+			Campo->Rows->Add();
+		}
+
+		Campo->ColumnHeadersVisible = false;
+		Campo->RowHeadersVisible = false;
+
+
+		for (int i = 0; i < Num_Fila; i++)
+		{
+			for (int j = 0; j < Num_Columns; j++)
+			{
+				Campo->Rows[i]->Cells[j]->Style->BackColor = Color::Green;
+			}
+		}
+
+		Campo->Rows[0]->Cells[0]->Value = "T";
+		Campo->Rows[1]->Cells[0]->Value = "H";
+
+		timer1->Enabled = true;
+
+	}
+		   
+		   
+		   int generarAtor(int limI, int limS) {
+			   int x;
+			   x = limI + rand() % (limS + 1 - limI);			   
+			   return x;
+		   }
+
+		   int generarAlib(int limIi, int limSs) {
+			   int  y;	  
+			   y = limIi + rand() % (limSs + 1 - limIi);
+			   return y;
+		   }
+
+		   int k = 0;
+		   int t, l;
+
+private: System::Void timer1_Tick(System::Object^ sender, System::EventArgs^ e) {
+	 
+		srand(time(NULL));
+		t = generarAtor(1, 10);
+		l = generarAlib(1, 10);
+
+	if (t == 1 || t == 2 || t == 3 || t == 4 || t == 5)
+	{
+		k++;
+		Campo->Rows[0]->Cells[k]->Value = "T";
+		Campo->Rows[0]->Cells[k - 1]->Value = " ";
+		
+	}
+	if (t == 6 || t == 7) {
+
+		if (k < 6)
+		{
+			if (k < 5)
+			{
+				if (k < 4)
+				{
+					if (k < 3)
+					{
+						if (k < 2)
+						{
+							if (k < 1)
+							{
+								Campo->Rows[0]->Cells[k]->Value = "T";
+							}
+							else {
+								k = k - 1;
+								Campo->Rows[0]->Cells[k]->Value = "T";
+								Campo->Rows[0]->Cells[k + 1]->Value = " ";
+							}
+						}
+						else {
+							k = k - 2;
+							Campo->Rows[0]->Cells[k]->Value = "T";
+							Campo->Rows[0]->Cells[k + 2]->Value = " ";
+						}
+					}
+					else {
+						k = k -3 ;
+						Campo->Rows[0]->Cells[k]->Value = "T";
+						Campo->Rows[0]->Cells[k + 3]->Value = " ";
+					}
+				}
+				else {
+					k = k - 4;
+					Campo->Rows[0]->Cells[k]->Value = "T";
+					Campo->Rows[0]->Cells[k + 4]->Value = " ";
+				}
+			}
+			else {
+				k = k - 5;
+				Campo->Rows[0]->Cells[k]->Value = "T";
+				Campo->Rows[0]->Cells[k + 5]->Value = " ";
+			}
+		}
+		else {
+        k = k - 6;
+		Campo->Rows[0]->Cells[k]->Value = "T";
+		Campo->Rows[0]->Cells[k + 6]->Value = " ";
+		}
+		
+		
+	}
+	if (t == 8 || t == 9 || t == 10) {
+
+		k = k + 3;
+		Campo->Rows[0]->Cells[k]->Value = "T";
+		Campo->Rows[0]->Cells[k - 3]->Value = " ";
+		
+	}
+
+
+
+
+
+
+
+    }
+};
 }
